1ca77b28625ffc582b633ee1a857e85d324d1007
Bianca Power <drbiancampower@gmail.com>
2018-05-08 21:13:04 +1000

    improve 1

--- 1-crack-the-code.js
+++ 1-crack-the-code.js
@@ -1,6 +1,12 @@
 // Crack the following code:
 // [77,97,103,105,99,32,97,110,100,32,109,97,121,104,101,109,33]
 
-answer = String.fromCharCode(77,97,103,105,99,32,97,110,100,32,109,97,121,104,101,109,33)
+let code = [77,97,103,105,99,32,97,110,100,32,109,97,121,104,101,109,33];
 
-console.log(answer)
+let answer = "";
+
+for (i = 0; i < code.length; i++) {
+    answer = answer.concat(String.fromCharCode(String(code[i])));
+}
+
+console.log(answer);
